7a58076b751f487265384ef69ee9e24162331421
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-12 14:22:06 +0900

    messages.properties作成

--- src/main/java/com/example/login/controller/SignupController.java
+++ src/main/java/com/example/login/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +44,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm signupForm, BindingResult result, Model model) {
+	public String postSignup(@ModelAttribute @Validated SignupForm signupForm, BindingResult result, Model model) {
 		
 		if (result.hasErrors()) {
 			return getSignup(signupForm, model);
